893d681cd7f5808d5e6ff1179ea167404d141169
lynndixon <ldixon@redhat.com>
2018-08-17 20:16:24 -0400

    Updated number of LEDS

--- live_sectional.py
+++ live_sectional.py
@@ -8,7 +8,7 @@ import datetime
 
 
 # LED strip configuration:
-LED_COUNT      = 88      # Number of LED pixels.
+LED_COUNT      = 115      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
