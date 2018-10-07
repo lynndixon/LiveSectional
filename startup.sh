9ad4f0dea066bd54e0e7608ff81247d6927ad08c
lynndixon <ldixon@redhat.com>
2018-10-06 23:47:57 -0400

    Final airports update

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 sudo echo -e "\nStarting up LiveSectional" >> /var/log/livesectional.log
 sudo python /LiveSectional/test.py
-sudo python /LiveSectional/live_sectional.py
+#sudo python /LiveSectional/live_sectional.py
 exit
