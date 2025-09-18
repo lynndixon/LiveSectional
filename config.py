84325ced134478a081d6f791f2fc5c1bef80002d
lynndixon <lynn@redhat.com>
2025-09-17 23:24:49 -0400

    Moved variables to config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+BASE_URL = "https://aviationweather.gov/api/data/metar?format=xml&ids="
+# LED strip configuration:
+LED_COUNT      = 136      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 16     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
